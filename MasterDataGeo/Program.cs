5ec15d973065694557cfd82a31b6713f7105c871
silentDoc <ramon78t@yahoo.es>
2023-04-19 13:40:02 +0200

    Reverse selection lookup system

--- MasterDataGeo/Program.cs
+++ MasterDataGeo/Program.cs
@@ -13,6 +13,11 @@
         static List<string> allTheCompanies = new();
         static List<string> allThePlants = new();
 
+        // Lookup tables for inverse selection
+        static Dictionary<string, string> LookupMarketSelection = new();
+        static Dictionary<string, (string, string)> LookupCountrySelection = new();
+        static Dictionary<string, (string, string, string)> LookupCompanySelection = new();
+
 
         static void Main(string[] args)
         {
@@ -28,11 +33,14 @@
             allThePlants = geographyElementsList.Select(x => x.DisplayPlant).Distinct().ToList();
 
             Console.WriteLine("Step 3 - Generate the Lookup tables for reverse selection");
-            
+            var elmentsUpToMarket = geographyElementsList.Select(x => (x.Zone, x.Market)).Distinct().ToList();
+            elmentsUpToMarket.ForEach(x => LookupMarketSelection[x.Market] = x.Zone);
 
+            var elementsUpToCountry = geographyElementsList.Select(x => (x.Zone, x.Market, x.Country)).Distinct().ToList();
+            elementsUpToCountry.ForEach(x => LookupCountrySelection[x.Country] = (x.Zone, x.Market));
 
-
-
+            var elementsUpToCompany = geographyElementsList.Select(x => (x.Zone, x.Market, x.Country, x.DisplayCompany)).Distinct().ToList();
+            elementsUpToCompany.ForEach(x => LookupCompanySelection[x.DisplayCompany] = (x.Zone, x.Market, x.Country));
         }
 
         static void LoadContentsFromDB()
@@ -42,5 +50,23 @@
 
             lines.ForEach(line => geographyElementsList.Add(new GeographyElement(line)));
         }
+
+        static (string, string, string, string) GetReverseFromPlant(string plantName)
+        {
+            var element = geographyElementsList.Where(x => x.DisplayPlant == plantName).FirstOrDefault();
+            return (element.Zone, element.Market, element.Country, element.DisplayCompany);
+        }
+
+        static (string, string, string) GetReverseFromCompany(string companyName)
+            => LookupCompanySelection[companyName];
+
+        static (string, string) GetReverseFromCountry(string countryName)
+            => LookupCountrySelection[countryName];
+
+        static string GetReverseFromMarket(string marketName)
+            => LookupMarketSelection[marketName];
+
+
+
     }
 }
