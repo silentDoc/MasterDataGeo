1927670af03a5b692b5c8f51d9bfd82032e71b4f
silentDoc <ramon78t@yahoo.es>
2023-04-19 13:08:21 +0200

    Agregar archivos de proyecto.

---
+++ MasterDataGeo/GeographyElement.cs
@@ -0,0 +1,30 @@
+﻿namespace MasterDataGeo
+{
+    public class GeographyElement
+    {
+        public string? Zone { get; set; }
+        public string? Market { get; set; }
+        public string? Country { get; set; }
+        public string? CompanyCode { get; set; }
+        public string? CompanyName { get; set; }
+        public string? PlantCode { get; set; }
+        public string? PlantName { get; set; }
+
+        public string DisplayCompany
+            => (CompanyCode ?? "") + " " + (CompanyName ?? "");
+
+        public string DisplayPlant
+            => (PlantCode ?? "") + " " + (PlantName ?? "");
+
+        public GeographyElement(string? zone, string? market, string? country, string? companyCode, string? companyName, string? plantCode, string? plantName)
+        {
+            Zone = zone;
+            Market = market;
+            Country = country;
+            CompanyCode = companyCode;
+            CompanyName = companyName;
+            PlantCode = plantCode;
+            PlantName = plantName;
+        }
+    }
+}
---
+++ MasterDataGeo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MasterDataGeo
+{
+    internal class Program
+    {
+        List<GeographyElement> geographyElements = new();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public LoadContentsFromDB()
+        { 
+
+        
+        }
+    }
+}
